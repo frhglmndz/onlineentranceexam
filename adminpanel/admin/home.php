2e643cc66287ddc769047fd2be6bf752fbb8d3a6
frhglmndz <157273331+frhglmndz@users.noreply.github.com>
2024-04-22 19:53:23 +0800

    Add files via upload

---
+++ adminpanel/admin/home.php
@@ -0,0 +1,72 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['admin']['adminnakalogin']) == true) header("location:index.php");
+
+
+ ?>
+<?php include("../../conn.php"); ?>
+<!-- ANG HEADER -->
+<?php include("includes/header.php"); ?>      
+
+<!-- UI THEME  -->
+<?php include("includes/ui-theme.php"); ?>
+
+<div class="app-main">
+<!-- sidebar  -->
+<?php include("includes/sidebar.php"); ?>
+
+
+
+<!--  -->
+<?php 
+   @$page = $_GET['page'];
+
+
+   if($page != '')
+   {
+     if($page == "add-course")
+     {
+     include("pages/add-course.php");
+     }
+     else if($page == "manage-course")
+     {
+     	include("pages/manage-course.php");
+     }
+     else if($page == "manage-exam")
+     {
+      include("pages/manage-exam.php");
+     }
+     else if($page == "manage-examinee")
+     {
+      include("pages/manage-examinee.php");
+     }
+     else if($page == "ranking-exam")
+     {
+      include("pages/ranking-exam.php");
+     }
+     else if($page == "feedbacks")
+     {
+      include("pages/feedbacks.php");
+     }
+     else if($page == "examinee-result")
+     {
+      include("pages/examinee-result.php");
+     }
+
+       
+   }
+   // Home page
+   else
+   {
+     include("pages/home.php"); 
+   }
+
+
+ ?> 
+
+
+<!-- footer -->
+<?php include("includes/footer.php"); ?>
+
+<?php include("includes/modals.php"); ?>
