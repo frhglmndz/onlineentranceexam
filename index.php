3d33aea164177a8e30840cf95ee0db11a62b58f8
frhglmndz <157273331+frhglmndz@users.noreply.github.com>
2024-04-22 20:45:17 +0800

    Update index.html

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="login-ui/image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/main.css">
+</head>
+<body>
+
+ <?php 
 session_start();
 if(isset($_SESSION['examineeSession']['examineenakalogin']) == true) header("location:home.php");
 
@@ -15,3 +33,6 @@ include("login-ui/index.php");
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/ajax.js"></script>
 <script type="text/javascript" src="js/sweetalert.js"></script>
+
+</body>
+</html>
